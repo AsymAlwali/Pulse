138894394aa921717dbcd8a98768dc3a9719087e
Asym Alwali <asymalwali101@gmail.com>
2026-04-06 16:56:56 +0100

    Update configure.js

--- configure.js
+++ configure.js
@@ -1,6 +1,6 @@
 // Initialize Supabase
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://okeimzznmvmxcehlhzrb.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rZWltenpubXZteGNlaGxoenJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0ODg2NzAsImV4cCI6MjA5MTA2NDY3MH0.VCnfyeSF6BFvnpxj6puTdAqEF_gyFLKVIzu4z6toz-0';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
